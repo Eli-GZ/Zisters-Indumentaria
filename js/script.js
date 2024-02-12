e3c9ac88a5f79b8290e7e66ad882d3c59b50f878
Elias <elias.d.gonzalez25@gmail.com>
2024-02-12 09:13:01 -0300

    media

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 //CARRUCEL
- const productContainers=[...document.querySelectorAll('.product-container')];
- const nxtBtn=[...document.querySelectorAll('.nxt-btn')];
- const preBtn=[...document.querySelectorAll('.pre-btn')];
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
- productContainers.forEach((item,i)=>{
-   let containerDimensions = item.getBoundingClientRect();
-   let containerwidth = containerDimensions.width;
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerwidth = containerDimensions.width;
+  let isScrolling = false;
 
-   nxtBtn[i].addEventListener('click',()=>{
-     item.scrollLeft += containerwidth;
+  nxtBtn[i].addEventListener('click', () => {
+    if (!isScrolling) {
+      isScrolling = true;
+      item.scrollLeft += containerwidth;
+     
+      nxtBtn[i].disabled = true;
+      setTimeout(() => {
+        isScrolling = false;
+        nxtBtn[i].disabled = false;
+      }, 500); 
+    }
+  });
 
-   })
-   preBtn[i].addEventListener('click',()=>{
-     item.scrollLeft -= containerwidth;
-    
-   })
-
-
- })
+  preBtn[i].addEventListener('click', () => {
+    if (!isScrolling) {
+      isScrolling = true;
+      item.scrollLeft -= containerwidth;
+      preBtn[i].disabled = true;
+      setTimeout(() => {
+        isScrolling = false;
+        preBtn[i].disabled = false;
+      }, 500); 
+    }
+  });
+});
