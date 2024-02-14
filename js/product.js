c71eaeff89eb75295b9d9a932259a2032bcb20c9
Elias <elias.d.gonzalez25@gmail.com>
2024-02-14 14:29:12 -0300

    contact

--- js/product.js
+++ js/product.js
@@ -3,7 +3,7 @@ document.addEventListener("keyup", e=>{
 
   if(e.target.matches("#buscador")){
 
-    document.querySelectorAll(".product-card").forEach(ropa=>{
+    document.querySelectorAll(".card-prd").forEach(ropa=>{
 
       ropa.textContent.toLowerCase().includes(e.target.value.toLowerCase())
       ? ropa.classList.remove("filtro") : ropa.classList.add("filtro")
