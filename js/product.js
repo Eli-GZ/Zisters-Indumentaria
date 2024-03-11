4a08436304bb8500901b8fc01fe3cfa666c38ce7
Elias <elias.d.gonzalez25@gmail.com>
2024-03-11 08:47:47 -0300

    media

--- js/product.js
+++ js/product.js
@@ -1,14 +1,14 @@
 //BUSCADOR
-document.addEventListener("keyup", e=>{
+document.addEventListener("keyup", e => {
 
-  if(e.target.matches("#buscador")){
+  if (e.target.matches("#buscador")) {
 
-    document.querySelectorAll(".card-prd").forEach(ropa=>{
+    document.querySelectorAll(".card-prd").forEach(ropa => {
 
       ropa.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-      ? ropa.classList.remove("filtro") : ropa.classList.add("filtro")
+        ? ropa.classList.remove("filtro") : ropa.classList.add("filtro")
     });
 
   }
-  
+
 });
